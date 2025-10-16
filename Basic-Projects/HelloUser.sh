a2b8c9b1b79c3ec7643cb42a8a50daf8cab17f4e
ranit1803 <ranitsantra.sssnc@gmail.com>
2025-10-16 04:22:50 +0000

    Committing First Project

---
+++ Basic-Projects/HelloUser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+<< comment 
+Takes your name as input and greets you dynamically.
+Learn: read, echo, variables.
+comment
+
+read -p "Enter Your Name: " name
+echo "Hello $name, Have a Great Day!"
